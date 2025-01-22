20bfd0de1f5ce0b2b3cb5d0fa09330b2ade2e23a
VitaliiBashkatov <bashkatov.vitalii@gmail.com>
2025-01-22 14:04:22 +0200

    Remove redundant code

--- api/src/user/dto/user.dto.ts
+++ api/src/user/dto/user.dto.ts
@@ -2,18 +2,10 @@ import { IsEnum, IsOptional, IsString } from 'class-validator'
 import { Expose } from 'class-transformer'
 import { Roles } from '@prisma/client'
 
-enum FilterUserWith {
-  ROLE = 'role',
-}
-
 export class FilterUserDto {
   @IsString()
   @IsOptional()
   email?: string
-
-  @IsEnum(FilterUserWith)
-  @IsOptional()
-  with?: FilterUserWith
 }
 
 export class UserDto {
--- api/src/user/user.controller.ts
+++ api/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common'
+import { Controller, Get, Post, Query } from '@nestjs/common'
 
 import { Serialize } from 'src/interceptors/serialize.interceptor'
 
